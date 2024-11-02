3e03043933a038fb36b52dbeed63849759460ce4
Morhush <sasha.morhush.z@gmail.com>
2024-11-02 14:51:01 +0200

    start project

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, session, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 import os
+
+from flask_sqlalchemy.session import Session
+
+SESSION_USER_ID = 'user_id'
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'school.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = '5RJyRE_DAeF1LyO8XAuyVUKZnkL9e2nALunIfp4YuK8'
 db = SQLAlchemy(app)
 
 
@@ -20,9 +25,17 @@ class User(db.Model):
     role = db.Column(db.Integer(), default=0, nullable=False)
 
     def __repr__(self):
-        pass
+        return f'<User: {self.username}>'
 
-# === ROUTS ===============
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+#створюєм базу даних
+with app.app_context():
+    db.create_all()
+
+
+# === ROUTS =============================================================
 
 @app.route('/')
 def index():
@@ -49,10 +62,33 @@ def pricing():
     return render_template('pricing.html')
 
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
+# --- ADMIN ---
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ''
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            message = 'Неправильний Email!'
+        else:
+            if user.check_password(password):
+                session[SESSION_USER_ID] = user.id
+                return redirect('/')
+
+            message = 'Неправильний пароль'
+
+    return render_template('login.html', message=message)
+
+@app.route('/logout')
+def logout():
+    session.pop(SESSION_USER_ID, None)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
